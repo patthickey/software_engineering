1efc9cf237286473996bdae31a9e39495f94702f
Patt Hickey <patthickey@gmail.com>
2015-10-27 18:32:06 -0400

    isolated the code from the sign up to a new sql calls php

--- sign_up.php
+++ sign_up.php
@@ -1,14 +1,6 @@
 <?php
 session_start();
-include 'db.inc';
-// Connect to MySQL DBMS
-if (!($connection = @ mysql_connect($hostName, $username,
-  $password)))
-  showerror();
-// Use the cars database
-if (!mysql_select_db($databaseName, $connection))
-  showerror();
- 
+include 'sql_calls.php';
 // Set variables from html page
 $email_up = $_POST['email_up'];
 $password1 = $_POST['password1'];
@@ -20,23 +12,7 @@ $SSN = $_POST['SSN'];
 $d_o_b = $_POST['d_o_b'];
 $date = date('Y-m-d');
 $privilege = 1; 
+	
+	sign_up($email_up, $hash, $first_name, $middle_name, $last_name, $SSN, $d_o_b, $privilege, $date);
 
-// Searching the email address in the database
-	$sql = "SELECT * FROM users WHERE email='$email_up'";
-	$result = mysql_query($sql);
-	$num_rows = mysql_num_rows($result);
-
-// Checking if the email address and adding accordingly, might need to check how that 1 is passed. Can't remember how to pass var
-	if($num_rows == 0){
-    	$query = "INSERT INTO users (email, password, first_name, middle_name, last_name, SSN, date_of_birth, privilege, join_date) VALUES ('$email_up', '$hash', '$first_name', '$middle_name', '$last_name', '$SSN', '$d_o_b','$privilege', '$date')";
-    	if (!($result = @ mysql_query ($query, $connection)))
-  	 	showerror();
-	} else {
-		echo '<script>';
-		echo 'alert("Email is already registered");';
-		echo 'location.href="index.html"';
-		echo '</script>';
-	}
-	header('Location:index.html');
-//echo('<META HTTP-EQUIV="Refresh" CONTENT="0; URL=index.html">');
 ?>
---
+++ sql_calls.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+// Connect to MySQL DBMS
+	
+	$connection;
+	$connected = False;
+
+	function connect_to_db(){
+		include 'db.inc';
+		if (!($GLOBALS['$connection'] = @ mysql_connect($hostName, $username,
+		  $password)))
+		  showerror();
+		// Use the cars database
+		if (!mysql_select_db($databaseName, $GLOBALS['$connection']))
+		  showerror();
+		$GLOBALS['$connected'] = True;
+	}
+	
+ 
+	function get_email($email)
+	{
+		if ($GLOBALS['$connected'] == False) 
+			connect_to_db();
+		$sql = "SELECT * FROM users WHERE email='$email'";
+		$result = mysql_query($sql);
+		return $result;
+	}
+
+
+	
+	function sign_up($email_up, $hash, $first_name, $middle_name, $last_name, $SSN, $d_o_b, $privilege, $date)
+	{
+
+		if ($GLOBALS['$connected'] == False) 
+			connect_to_db();
+	// Searching the email address in the database
+		
+		$result = get_email($email_up);
+		$num_rows = mysql_num_rows($result);
+
+	// Checking if the email address and adding accordingly, might need to check how that 1 is passed. Can't remember how to pass var
+		if($num_rows == 0){
+	    	$query = "INSERT INTO users (email, password, first_name, middle_name, last_name, SSN, date_of_birth, privilege, join_date) VALUES ('$email_up', '$hash', '$first_name', '$middle_name', '$last_name', '$SSN', '$d_o_b','$privilege', '$date')";
+	    	if (!($result = @ mysql_query ($query, $GLOBALS['$connection'])))
+	  	 	showerror();
+		} else {
+			echo '<script>';
+			echo 'alert("Email is already registered");';
+			echo 'location.href="index.html"';
+			echo '</script>';
+		}
+		header('Location:index.html');
+	}
+	
+
+?>
