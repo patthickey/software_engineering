8bba762be9cead5c8448fa5feea7f3bf08146e54
Austin Layne <wlayne94@yahoo.com>
2015-11-20 00:37:07 -0500

    Added functionality to the contact us page. New function in SQL calls, new contact.php file, and made the contact_us.html reference the contact.php page.

---
+++ contact.php
@@ -0,0 +1,71 @@
+<?php session_start(); 
+include 'sql_calls.php';
+//$year = $_GET["year"];
+//$email = $_GET["email"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Patt Hickey, Victor Adesanmi, Austin Layne, Zachary Kombet">  
+    <title>IRS (mock site)</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+
+  <body>
+    <div id="headerfile"></div>   
+    <div class="container-fluid"> 
+    <div class="row">
+      <div class="col-md-10 col-md-offset-1">
+
+                
+        <?php
+        $fname = $_POST['fname'];
+        $email = $_POST['email'];
+        $lname = $_POST['lname'];
+        $msg = $_POST['message'];
+        $phone = $_POST['phone'];
+        
+        
+        contact_us($fname,$lname,$email,$phone,$msg); 
+        ?>
+
+      </div>
+    </div>
+    </div>
+    <div id="footerfile"></div>
+
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-68434893-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script type="text/JavaScript" src="IRS_mock_site.js"></script> 
+    <script> 
+      $(function(){
+        $("#headerfile").load("header.html"); 
+        $("#footerfile").load("footer.html"); 
+      });
+    </script>
+
+  </body>
+</html>
